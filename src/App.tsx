f15dae969300353d6875d887807721c909cc6f8e
Kouvousis <dexous1996@gmail.com>
2025-05-20 21:05:33 +0300

    chore: code readability

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,8 @@ function App() {
         <ArrowFunctionalComponentWithProps title={"Is a Arrow Functional Component with Props"} />
         <ArrowFunctionalComponentWithPropsType
             title={"Is a Arrow Functional Component with Props and Types"}
-            description={"This is a description"} />
+            description={"This is a description"}
+        />
     </>
   )
 }
