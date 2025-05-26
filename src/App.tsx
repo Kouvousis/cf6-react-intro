be0deff8df579d44bc8e4eb6fcab18e9d7093764
Kouvousis <dexous1996@gmail.com>
2025-05-26 18:17:24 +0300

    chore: commented some components

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import ClassComponent from "./components/ClassComponent.tsx";
-import FunctionalComponent from "./components/FunctionalComponent.tsx";
-import ArrowFunctionalComponent from "./components/ArrowFunctionalComponent.tsx";
-import ArrowFunctionalComponentWithProps from "./components/ArrowFunctionalComponentWithProps.tsx";
-import ArrowFunctionalComponentWithPropsType from "./components/ArrowFunctionalComponentWithPropsType.tsx";
+// import ClassComponent from "./components/ClassComponent.tsx";
+// import FunctionalComponent from "./components/FunctionalComponent.tsx";
+// import ArrowFunctionalComponent from "./components/ArrowFunctionalComponent.tsx";
+// import ArrowFunctionalComponentWithProps from "./components/ArrowFunctionalComponentWithProps.tsx";
+// import ArrowFunctionalComponentWithPropsType from "./components/ArrowFunctionalComponentWithPropsType.tsx";
 import Layout from "./components/Layout.tsx";
 // import FunctionalComponentWithState from "./components/FunctionalComponentWithState.tsx";
 import Counter from "./components/Counter.tsx";
@@ -14,14 +14,14 @@ function App() {
         <>
 
             <Layout>
-                <ClassComponent/>
-                <FunctionalComponent/>
-                <ArrowFunctionalComponent/>
-                <ArrowFunctionalComponentWithProps title={"Is a Arrow Functional Component with Props"}/>
-                <ArrowFunctionalComponentWithPropsType
-                    title={"Is a Arrow Functional Component with Props and Types"}
-                    description={"This is a description"}
-                />
+                {/*<ClassComponent/>*/}
+                {/*<FunctionalComponent/>*/}
+                {/*<ArrowFunctionalComponent/>*/}
+                {/*<ArrowFunctionalComponentWithProps title={"Is a Arrow Functional Component with Props"}/>*/}
+                {/*<ArrowFunctionalComponentWithPropsType*/}
+                {/*    title={"Is a Arrow Functional Component with Props and Types"}*/}
+                {/*    description={"This is a description"}*/}
+                {/*/>*/}
                 {/*<ClassComponentWithState/>*/}
                 {/*<FunctionalComponentWithState/>*/}
                 <Counter/>
