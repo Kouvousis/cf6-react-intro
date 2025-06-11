cff1871702cb2fbb0205668a288b6c058f23b7a9
Kouvousis <dexous1996@gmail.com>
2025-06-11 19:20:37 +0300

    feat: add MultiFieldForm component with html validation

--- src/App.tsx
+++ src/App.tsx
@@ -17,18 +17,19 @@
 // import CounterWithAdvancedCustomHooks from "./components/CounterWithAdvancedCustomHook.tsx";
 // import ClassComponentWithState from "./components/ClassComponentWithState.tsx";
 import {BrowserRouter, Routes, Route} from "react-router";
-// import HomePage from "./pages/HomePage.tsx";
+import HomePage from "./pages/HomePage.tsx";
 import NameChangerPage from "./pages/NameChangerPage.tsx";
 import OnlineStatusPage from "./pages/OnlineStatusPage.tsx";
 import UserPage from "./pages/UserPage.tsx";
 import RouterLayout from "./components/RouterLayout.tsx";
-import ExamplesPage from "./pages/ExamplesPage.tsx";
+// import ExamplesPage from "./pages/ExamplesPage.tsx";
 import RouterExamplesLayout from "./components/RouterExamplesLayout.tsx";
 // import AutoRedirectPage from "./pages/AutoRedirectPage.tsx";
 import AdvancedRedirect from "./components/AdvancedRedirect.tsx";
 import NotFoundPage from "./pages/NotFoundPage.tsx";
 // import FocusInput from "./components/FocusInput.tsx";
 import UncontrolledInput from "./components/UncontrolledInput.tsx";
+import MultiFieldForm from "./components/MultiFieldForm.tsx";
 
 function App() {
     // useEffect(() => {
@@ -65,15 +66,15 @@ function App() {
             <BrowserRouter>
                 {/*<Layout>*/}
                 <Routes>
-                    {/*<Route path="/" element={<HomePage/>}/>*/}
+                    <Route path="/" element={<HomePage/>}/>
                     <Route element={<RouterLayout/>}>
                         {/*<Route index element={<HomePage/>}/>*/}
+                        <Route index element={<MultiFieldForm/>}/>
                         {/*<Route index element={<FocusInput/>}/>*/}
                         <Route index element={<UncontrolledInput/>}/>
-
                     </Route>
                     <Route path="examples" element={<RouterExamplesLayout/>}>
-                        <Route index element={<ExamplesPage/>}/>
+                        {/*<Route index element={<ExamplesPage/>}/>*/}
                         <Route path="name-changer" element={<NameChangerPage/>}></Route>
                         <Route path="online-status" element={<OnlineStatusPage/>}></Route>
                         {/*<Route path="auto-redirect" element={<AutoRedirectPage/>}/>*/}
---
+++ src/components/MultiFieldForm.tsx
@@ -0,0 +1,104 @@
+import {useState} from "react";
+
+type FormValues = {
+    name: string,
+    email: string,
+    message: string,
+}
+
+const initialValues = {
+    name: "",
+    email: "",
+    message: "",
+}
+
+const MultiFieldForm = () => {
+    const [values, setValues] = useState<FormValues>(initialValues);
+    const [suibmitedData, setSuibmittedData] = useState<FormValues | null>(null);
+
+    const handleSubmit = (e: React.FormEvent) => {
+        e.preventDefault();
+        setSuibmittedData(values)
+        console.log(values)
+        setValues(initialValues);
+    }
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+        const {name, value} = e.target;
+        setValues(
+            (prev => ({
+                    ...prev,
+                    [name]: value
+                })
+            )
+        )
+    }
+
+    const handleClear = () => {
+        setValues(initialValues);
+        setSuibmittedData(null);
+    }
+
+    return (
+        <>
+            <div className="flex mx-auto max-w-md mt-8 ">
+                <form onSubmit={handleSubmit} className="space-y-4">
+                    <input
+                        type="text"
+                        name="name"
+                        value={values.name}
+                        placeholder="Name"
+                        onChange={handleChange}
+                        className="w-full px-4 py-2 rounded border "
+                        required
+                    />
+                    <input
+                        type="email"
+                        name="email"
+                        value={values.email}
+                        placeholder="Email"
+                        onChange={handleChange}
+                        className="w-full px-4 py-2 rounded border "
+                        required
+                    />
+                    <textarea
+                        name="message"
+                        value={values.message}
+                        className="w-full px-4 py-2 rounded border "
+                        placeholder="Type your message"
+                        onChange={handleChange}
+                        required
+                    >
+                </textarea>
+                    <div className="flex gap-4 ">
+                        <button
+                            type="submit"
+                            className="bg-cf-dark-red text-white px-4 py-2 rounded"
+                        >
+                            Submit
+                        </button>
+                        <button
+                            type="button"
+                            className="bg-cf-gray text-white px-4 py-2 rounded"
+                            onClick={handleClear}
+                        >
+                            Clear
+                        </button>
+                    </div>
+
+                    {suibmitedData && (
+                        <div className="mt-6 border-t pt-4 space-y-2">
+                            <h2 className="">Submitted Data</h2>
+                            <p><strong>Name:</strong> {suibmitedData.name}</p>
+                            <p><strong>Email:</strong> {suibmitedData.email}</p>
+                            <p><strong>Message:</strong> {suibmitedData.message}</p>
+                        </div>
+                    )}
+
+                </form>
+            </div>
+        </>
+    )
+}
+
+export default MultiFieldForm
